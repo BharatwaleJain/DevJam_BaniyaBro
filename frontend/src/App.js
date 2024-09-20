e2e2d5d4f9fbd952c13a73ccaf347c61ad1487c4
ruchit2005 <ruchitgupta05@gmail.com>
2024-09-21 03:25:46 +0530

    component test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,16 @@ import logo from './logo.svg';
 import Head from './MyComponents/header/head';
 import ImageUploader from './MyComponents/imagein/imagein';
 import LoadingScreen from './MyComponents/LoadingScreen';
+import Comp from './MyComponents/diffitems/diffitems';
 
 function App() {
   return (
     <Router>
       <Head />
+      <Comp/>
       <div className='board'>
+       
+        
       <Routes>
         <Route path="/" element={<LoadingScreen />} />
         <Route path="/upload" element={<ImageUploader />} />
