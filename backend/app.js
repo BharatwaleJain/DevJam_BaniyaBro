a8b7f019a44370f08083ae0a83bf16e354ad74c9
KushalAgrawal30 <kushalagrawal3011@gmail.com>
2024-09-20 16:47:08 +0530

    fixes

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const mainRouter = require('./router/mainRouter')
+
 app = express();
 
 
+app.use(express.json())
+app.use(cors())
 app.use('/', mainRouter)
 
 module.exports = app;
