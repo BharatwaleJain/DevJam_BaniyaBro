dc7a97f32a63886f6a8a2d2227a253b3afa95fa9
KushalAgrawal30 <kushalagrawal3011@gmail.com>
2024-09-20 16:29:23 +0530

    backend update

--- backend/controllers/mainControllers.js
+++ backend/controllers/mainControllers.js
@@ -2,7 +2,6 @@ const fs = require('fs');
 const { getJson } = require("serpapi");
 const User = require('../models/model');
 
-
 exports.home = async (req,res) => {
     try{
        res.status(200).send("HOME PAGE"); 
@@ -19,7 +18,7 @@ exports.getLens = async (req,res) => {
     try{
         getJson({
             engine: "google_lens",
-            url: "https://i.imgur.com/HBrB8p0.png",
+            url: "https://th.bing.com/th/id/OIP.RclgOCmXMr2z57kL5O3onwHaFJ?rs=1&pid=ImgDetMain",
             api_key: "e46eb16e06b86f316f7c4fcb0059c24f949e1cec889d9dcbdfc087f140452d40"
         }, (json) => {
             fs.writeFileSync('lensdata.json',JSON.stringify(json["visual_matches"]));
