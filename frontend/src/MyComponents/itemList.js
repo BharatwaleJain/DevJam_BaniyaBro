83a4c49756f86473cf6e90a4276549b7d28893a9
bharatwalejain <aarjavkjain2@gmail.com>
2024-09-21 23:16:36 +0530

    fix: update itemList.js

--- frontend/src/MyComponents/itemList.js
+++ frontend/src/MyComponents/itemList.js
@@ -1,17 +1,38 @@
-import React from 'react';
+import React,{ useState, useEffect } from 'react';
 import Item from './items';
+import axios from 'axios'
 import data from './Assets/sample.json';
 
 const ItemList = () => {
+  const [items, setItems] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    axios.get('http://localhost:8000/getData')
+      .then((response) => {
+        console.log("This is the respone",response)
+        setItems(response.data.data);
+      })
+      .catch((error) => {
+        console.error("API fetch failed, using fallback data", error);
+        setItems(data.data);
+        setError("Failed to load data from API");
+      })
+      .finally(() => {
+        setLoading(false);
+      });;
+  }, []);
+
   return (
     <div>
       {data.items.map((item, index) => (
         <Item
           key={index}
-          link={item.link}
-          name={item.name}
+          link={item.prodURL}
+          name={item.title.length > 15 ? item.title.slice(0, 12) + '...' : item.title}
           price={item.price}
-          imgSrc={item.imgSrc}
+          imgSrc={item.imgUrl}
         />
       ))}
     </div>
