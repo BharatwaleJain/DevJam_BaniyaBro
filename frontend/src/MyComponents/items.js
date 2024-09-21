a91259d7a06382a7007aa4a7e1d5b9aeb566b07b
bharatwalejain <aarjavkjain2@gmail.com>
2024-09-21 16:22:05 +0530

    fix: update item.js

--- frontend/src/MyComponents/items.js
+++ frontend/src/MyComponents/items.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import "./global.css";
+import axios from 'axios';
 
-const Item = () => {
-  const link = "https://www.amazon.com";
+const Item = ({ link, name, price, imgSrc }) => {
   return (
     <a href={link} target="_blank" rel="noopener noreferrer" className='card-link'>
       <div className='card'>
-        <img src='https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg' alt='Product'/>
+        <img src={imgSrc} alt='Product'/>
         <div className='details'>
-          <h1>Product Name</h1>
-          <div className='price'><h3>Price</h3></div>
+          <h1>{name}</h1>
+          <div className='price'><h3>{price}</h3></div>
         </div>
       </div>
     </a>
