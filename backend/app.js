7b29714b900c0b3f378faf475fab0c9ce60c5950
bharatwalejain <aarjavkjain2@gmail.com>
2024-09-22 02:47:03 +0530

    fix: backend fix

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,12 @@
 const express = require('express');
 const cors = require('cors');
 const mainRouter = require('./router/mainRouter');
-const tracker = require('./tracker/tracker')
 
 app = express();
-
 
 app.use(express.json())
 app.use(cors())
 
-
 app.use('/',mainRouter)
 
-tracker.printdata();
-
 module.exports = app;
-
