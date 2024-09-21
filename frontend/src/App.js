074fa8a626efe5f549ca29ce6ef793ed933758d5
bharatwalejain <aarjavkjain2@gmail.com>
2024-09-22 00:54:44 +0530

    fix: update bg & routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,39 +1,62 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import Head from './MyComponents/header/head';
+import './MyComponents/global.css';
 import ImageUploader from './MyComponents/imagein/imagein';
-import LoadingScreen from './MyComponents/LoadingScreen';
-import Item from './MyComponents/items';
+import Loader from './MyComponents/Loader';
+import Logo from './MyComponents/Logo';
+import Email from './MyComponents/Email';
+import Header from './MyComponents/Header';
 import ItemList from './MyComponents/itemList';
 import Price from './MyComponents/Price';
+import bgVideo from './MyComponents/Assets/bgvideo.mp4';
 
 function App() {
+  const videoRef = useRef(null);
+  useEffect(() => {
+    if (videoRef.current) {
+      videoRef.current.playbackRate = 0.8;
+    }
+  }, []);
+
   return (
-    <Router>
-        <Routes>
-          <Route path="/" element={
-            <div className="board">
-              <LoadingScreen />
-            </div>} />
-          <Route path="/upload" element={
-            <>
-            <Head />
-            <div className="board">
-              <ImageUploader />
-            </div>
-            </>} />
-          <Route path="/list" element={
-            <>
-            <Head />
-            <div id='contain'>
-              <Price />
-              <ItemList />
-            </div>
-            </>
-          } />
-        </Routes>
-    </Router>
+    <div className="App">
+      <video ref={videoRef} autoPlay loop muted id="bgvideo">
+        <source src={bgVideo} type="video/mp4" />
+      </video>
+      <Router>
+          <Routes>
+            <Route path="/start" element={
+              <div className="board">
+                <Loader />
+              </div>} />
+            <Route path="/" element={
+              <div className="board">
+                <div className="instructions">
+                  <Logo />
+                  <br/><br/>
+                  <Email />
+                </div>
+              </div>} />
+            <Route path="/upload" element={
+              <>
+              <Header />
+              <div className="board">
+                <ImageUploader />
+              </div>
+              </>} />
+            <Route path="/list" element={
+              <>
+              <Header />
+              <div id='contain'>
+                <Price />
+                <ItemList />
+              </div>
+              </>
+            } />
+          </Routes>
+      </Router>
+    </div>
     );
 }
 
