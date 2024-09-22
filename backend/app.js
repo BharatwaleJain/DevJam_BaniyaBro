d31a5d5d52dcb4bc0e4d0a0d3b34ed977c4d6fe3
bharatwalejain <aarjavkjain2@gmail.com>
2024-09-22 10:20:03 +0530

    init: cors

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,33 @@
+// const express = require('express');
+// const cors = require('cors');
+// const mainRouter = require('./router/mainRouter');
+
+// app = express();
+
+// app.use(express.json())
+// app.use(cors({
+//     origin:'https://baniyabro.onrender.com/input-price'
+// ))
+// }
+    
+
+// app.use('/',mainRouter)
+
+// module.exports = app;
+
+
 const express = require('express');
 const cors = require('cors');
 const mainRouter = require('./router/mainRouter');
 
-app = express();
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
 
-app.use('/',mainRouter)
+app.use(cors({
+    origin: 'https://baniyabro.onrender.com'
+}));
+
+app.use('/', mainRouter);
 
 module.exports = app;
